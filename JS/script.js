870041f61286e91208294906ef56862ef0652b9c
stefanoPrimo <36199751+stefanoPrimo@users.noreply.github.com>
2021-06-01 12:56:53 +0200

    fixes

---
+++ JS/script.js
@@ -0,0 +1,72 @@
+var $table = $('#RicevuteTbl')
+var $form = $('#RicevuteForm')
+var index = 0;
+$(function() {
+    $table.bootstrapTable();
+    $("#datepicker").datepicker({
+        dateFormat: 'dd/mm/yy'
+    });
+
+    $form.on("submit", (event) => {
+        event.preventDefault();
+        var data = getFormData($form);
+        console.log(data);
+        data.nr = BuildNos(data.data);
+        $table.bootstrapTable('insertRow', {
+            index: index,
+            row: data
+        });
+        index++;
+        return false;
+    })
+
+    $("#printRicevute").on("click", () => {
+        var data = $table.bootstrapTable('getData');
+        $("#Links").empty();
+        var template = $("#template").clone().removeAttr("style").html();
+        var $content = $("#all");
+
+        for (var element in data) {
+            var edited = template
+            Object.keys(data[element]).forEach(val => {
+                edited = edited.replaceAll("{{" + val + "}}", data[element][val]);
+            })
+            $content.append(edited)
+        };
+        var res = document.getElementById('all').getElementsByClassName('Content')
+        Array.prototype.forEach.call(res, function(elem) {
+            html2canvas(elem).then(function(canvas) {
+                $("#Links").append("<a download='" + elem.getAttribute("data-id") + "' href='" + canvas.toDataURL() + "'>" + elem.getAttribute("data-id") + "</a><br>");
+                elem.remove();
+            });
+        });
+
+    });
+    $("#DownloadAll").on("click", () => {
+        var ele = $("#Links a");
+        ele.each(elem => ele[elem].click())
+    });
+
+
+})
+
+function BuildNos(data) {
+    var splitted = data.split("/");
+    return splitted[2] + "-" + splitted[1] + "-" + pad(index, 4)
+}
+
+function pad(str, max) {
+    str = str.toString();
+    return str.length < max ? pad("0" + str, max) : str;
+}
+
+function getFormData($form) {
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i) {
+        indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+}
