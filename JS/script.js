28a675241657ce652761e3eecf3e96229b5aa983
stefanoPrimo <36199751+stefanoPrimo@users.noreply.github.com>
2021-11-24 10:26:04 +0100

    New layout - Full Bootrstrap template

--- JS/script.js
+++ JS/script.js
@@ -8,19 +8,6 @@ $(function() {
     canvas = document.querySelector("canvas");
     signaturePad = new SignaturePad(canvas);
 
-    /* function resizeCanvas() {
-         // When zoomed out to less than 100%, for some very strange reason,
-         // some browsers report devicePixelRatio as less than 1
-         // and only part of the canvas is cleared then.
-         var ratio = Math.max(window.devicePixelRatio || 1, 1);
-         canvas.width = canvas.offsetWidth * ratio;
-         canvas.height = canvas.offsetHeight * ratio;
-         canvas.getContext("2d").scale(ratio, ratio);
-     }
-
-     window.onresize = resizeCanvas;
-     resizeCanvas();*/
-
     let OldSignature = localStorage.getItem("Signature");
     if (OldSignature) {
         signaturePad.fromDataURL(OldSignature);
